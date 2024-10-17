d809b7dbf71cd631da9e81248691b6f3af3e7a2e
Quentin Brooks <101833847+Anqui3tas@users.noreply.github.com>
2024-10-17 09:08:50 -0700

    TMDB Poster Script fix
    -Updated TMDB poster background script to include the use of a separate file for the API key (in the event they add more functionality)
    -Added more notes to the script to explain what it is doing
    -Removed backup of previous site

--- get-movies.php
+++ get-movies.php
@@ -1,7 +1,11 @@
 <?php
 // get-movies.php
 
-$apiKey = 'APIKEY';
+header("Access-Control-Allow-Origin: *"); // Allow any origin
+header("Access-Control-Allow-Methods: GET"); // Allow GET requests
+header("Access-Control-Allow-Headers: Content-Type"); // Allow specific headers
+
+$apiKey = 'YOUR_TMDB_API_KEY_HERE';
 $apiURL = "https://api.themoviedb.org/3/trending/all/day?api_key={$apiKey}";
 
 // Fetch data from the TMDB API
@@ -10,3 +14,4 @@ $response = file_get_contents($apiURL);
 // Return the response to the frontend
 header('Content-Type: application/json');
 echo $response;
+?>
